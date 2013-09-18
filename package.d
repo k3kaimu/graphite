fd811b94a37085e0c45eb16d5c5e3e3545e698be
k3_kaimu <enjouzensyou.boinc@gmail.com>
2013-09-18 14:31:10 +0900

    package.dの追加

---
+++ package.d
@@ -0,0 +1,16 @@
+module graphite;
+
+public import graphite.addons,
+              graphite.app,
+              graphite.communication,
+              //graphite.deimos,
+              graphite.events,
+              graphite.gl,
+              graphite.graphics,
+              graphite.math,
+              graphite.r2,
+              graphite.r3,
+              graphite.sound,
+              graphite.types,
+              graphite.utils,
+              graphite.video;
