994566861dccb21e263f68c6addf5c811b5d3c2e
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-01-02 00:56:34 +0900

    utils.logを追加

---
+++ utils/log.d
@@ -0,0 +1,213 @@
+module graphite.utils.log;
+
+import std.range;
+import std.format;
+import std.stdio;
+
+struct Logger
+{
+    enum Level
+    {
+        verbose,
+        notice,
+        warning,
+        error,
+        fatalError,
+        silent,
+    }
+
+
+    void write(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    {
+        if(lv >= _currLv){
+            _orng.formattedWrite("[%s]<%s>{%s(%s): %s}: ", lv, _id, file, line, fn);
+
+            foreach(ref e; args)
+                _orng.formattedWrite("%s", e);
+        }
+    }
+
+
+    void write(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    {
+        write!(mixin(`Level.` ~ lv), file, line, fn)(args);
+    }
+
+
+    void writef(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(in char[] format, T args)
+    {
+        if(lv >= _currLv){
+            this.write!(lv, file, line, fn)();
+            _orng.formattedWrite(format, args);
+        }
+    }
+
+
+    void writef(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(in char[] format, T args)
+    {
+        writef!(mixin(`Level.` ~ lv), file, line, fn)(format, args);
+    }
+
+
+    void writeln(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    {
+        this.write!(lv, file, line, fn)(args, '\n');
+    }
+
+
+    void writeln(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    {
+        writeln!(mixin(`Level.` ~ lv), file, line, fn)(args);
+    }
+
+
+    void writefln(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(string format, T args)
+    {
+        this.writef!(lv, file, line, fn)(format ~ '\n', args);
+    }
+
+
+    void writefln(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(string format, T args)
+    {
+        write!(mixin(`Level.` ~ lv), file, line, fn)(format, args);
+    }
+
+
+    Level currLevel() @property
+    {
+        return _currLv;
+    }
+
+
+    void currLevel(Level level) @property
+    {
+        this._currLv = level;
+    }
+
+
+    void bindFile(string filename)
+    {
+        bindFile(File(filename, "w"));
+    }
+
+
+    void bindFile(File f)
+    {
+        bind(f.lockingTextWriter);
+    }
+
+
+    void bindConsole()
+    {
+        bindFile(stdout);
+    }
+
+
+    void bind(Writer)(Writer range)
+    {
+        _orng = makeORng(range);
+    }
+
+
+  private:
+    static auto makeORng(T)(T outputRange)
+    {
+        return outputRangeObject!dchar(outputRange);
+    }
+
+    string _id;
+
+    
+    OutputRange!dchar _orng;
+    
+    Level _currLv;
+}
+
+
+private Logger[] loggers;
+
+
+static this()
+{
+    loggers ~= Logger("Global");
+}
+
+
+ref Logger logger() @property
+{
+    return loggers[0];
+}
+
+
+ref Logger logger(alias identifier)() @property
+{
+    static size_t index;
+
+    if(index == 0){
+        loggers ~= loggers[0];
+        loggers[$-1]._id = __traits(identifier, identifier);
+        index = loggers.length - 1;
+    }
+
+    return loggers[index];
+}
+
+
+auto allLogger() @property
+{
+    struct AllLogger
+    {
+        void currLevel(Logger.Level lv)
+        {
+            foreach(ref e; loggers)
+                e.currLevel = lv;
+        }
+
+
+        void bindFile(string filename)
+        {
+            foreach(ref e; loggers)
+                e.bindFile(filename);
+        }
+
+
+        void bindFile(File f)
+        {
+            foreach(ref e; loggers)
+                e.bindFile(f);
+        }
+
+
+        void bindConsole()
+        {
+            foreach(ref e; loggers)
+                e.bindConsole();
+        }
+
+
+        void bind(Writer)(Writer range)
+        {
+            foreach(ref e; loggers)
+                e.bind(range);
+        }
+    }
+}
+
+
+unittest
+{
+    import std.array;
+
+    auto app = appender!string();
+    logger!(graphite.utils.log).bind(app);
+    logger!(graphite.utils.log).currLevel = Logger.Level.warning;
+
+    logger!(graphite.utils.log).write!("warning", "file", 0, "func")("foo");
+    assert(app.data == `[warning]<log>{file(0): func}: foo`);
+
+    logger!(graphite.utils.log).write!("notice", "file", 1, "func")("bar");
+    assert(app.data == `[warning]<log>{file(0): func}: foo`);
+
+    logger!(graphite.utils.log).write!("fatalError", "f", 2, "p")("hoge");
+    assert(app.data == "[warning]<log>{file(0): func}: foo[fatalError]<log>{f(2): p}: hoge");
+}
