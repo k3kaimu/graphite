65a2fdcae48dbecaa39406a648c4338d66cc90a8
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-01-04 13:28:35 +0900

    graphite.utils.package.dを更新

--- utils/package.d
+++ utils/package.d
@@ -1,4 +1,6 @@
 module graphite.utils;
 
 public import graphite.utils.constants,
-              graphite.utils.noise;
+              graphite.utils.log,
+              graphite.utils.noise,
+              graphite.utils.thread;
