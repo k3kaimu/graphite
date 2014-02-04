ffa0fb1cda846dd01f15db57a5c7915aabcecd55
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-02-04 20:07:27 +0900

    graphite.utils.logの変更
    使いやすく変更

--- source/graphite/utils/log.d
+++ source/graphite/utils/log.d
@@ -4,23 +4,35 @@ import std.range;
 import std.format;
 import std.stdio;
 
+
+struct LogElement
+{
+    Level lv;
+    string id;
+    string file;
+    string line;
+    string func;
+    string msg;
+}
+
+
+enum Level
+{
+    verbose,
+    notice,
+    warning,
+    error,
+    fatalError,
+    silent,
+}
+
+
 struct Logger
 {
-    enum Level
-    {
-        verbose,
-        notice,
-        warning,
-        error,
-        fatalError,
-        silent,
-    }
-
-
-    void write(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    void write(Level lv, T...)(T args)
     {
         if(lv >= _currLv){
-            _orng.formattedWrite("[%s]<%s>{%s(%s): %s}: ", lv, _id, file, line, fn);
+            _orng.formattedWrite("[%s]<%s>{%s(%s): %s}: ", lv, _id, _file, _line, _func);
 
             foreach(ref e; args)
                 _orng.formattedWrite("%s", e);
@@ -28,48 +40,48 @@ struct Logger
     }
 
 
-    void write(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    void write(string lv, T...)(T args)
     {
-        write!(mixin(`Level.` ~ lv), file, line, fn)(args);
+        write!(mixin(`Level.` ~ lv))(args);
     }
 
 
-    void writef(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(in char[] format, T args)
+    void writef(Level lv, T...)(in char[] format, T args)
     {
         if(lv >= _currLv){
-            this.write!(lv, file, line, fn)();
+            this.write!(lv)();
             _orng.formattedWrite(format, args);
         }
     }
 
 
-    void writef(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(in char[] format, T args)
+    void writef(string lv, T...)(in char[] format, T args)
     {
-        writef!(mixin(`Level.` ~ lv), file, line, fn)(format, args);
+        writef!(mixin(`Level.` ~ lv))(format, args);
     }
 
 
-    void writeln(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    void writeln(Level lv, T...)(T args)
     {
-        this.write!(lv, file, line, fn)(args, '\n');
+        this.write!(lv)(args, '\n');
     }
 
 
-    void writeln(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(T args)
+    void writeln(string lv, T...)(T args)
     {
-        writeln!(mixin(`Level.` ~ lv), file, line, fn)(args);
+        writeln!(mixin(`Level.` ~ lv))(args);
     }
 
 
-    void writefln(Level lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(string format, T args)
+    void writefln(Level lv, T...)(string format, T args)
     {
-        this.writef!(lv, file, line, fn)(format ~ '\n', args);
+        this.writef!(lv)(format ~ '\n', args);
     }
 
 
-    void writefln(string lv, string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__, T...)(string format, T args)
+    void writefln(string lv, T...)(string format, T args)
     {
-        write!(mixin(`Level.` ~ lv), file, line, fn)(format, args);
+        write!(mixin(`Level.` ~ lv))(format, args);
     }
 
 
@@ -115,9 +127,22 @@ struct Logger
         return outputRangeObject!dchar(outputRange);
     }
 
+
+    void setEnv(string file, size_t line, string func)
+    {
+        _file = file;
+        _line = line;
+        _func = func;
+    }
+
+
     string _id;
     OutputRange!dchar _orng;
     Level _currLv;
+
+    string _file;
+    size_t _line;
+    string _func;
 }
 
 
@@ -126,30 +151,32 @@ private Logger[] loggers;
 
 static this()
 {
-    loggers ~= Logger("Global", null, Logger.Level.verbose);
+    loggers ~= Logger("Global", null, Level.verbose);
     loggers[0].bindConsole;
 
     logger!"unittest".bindConsole();
-    logger!"unittest".thrLv = Logger.Level.warning;
+    logger!"unittest".thrLv = Level.warning;
 }
 
 
-ref Logger logger() @property
+ref Logger originLogger(string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__)
 {
+    loggers[0].setEnv(file, line, fn);
     return loggers[0];
 }
 
 
-ref Logger logger(alias identifier)() @property
+ref Logger logger(alias identifier = __MODULE__)(string file = __FILE__, size_t line = __LINE__, string fn = __PRETTY_FUNCTION__)
 {
     static size_t index;
 
     if(index == 0){
         loggers ~= loggers[0];
-        loggers[$-1]._id = __traits(identifier, identifier);
+        loggers[$-1]._id = identifier.stringof;
         index = loggers.length - 1;
     }
 
+    loggers[index].setEnv(file, line, fn);
     return loggers[index];
 }
 
@@ -158,7 +185,7 @@ auto allLogger() @property
 {
     struct AllLogger
     {
-        void thrLv(Logger.Level lv)
+        void thrLv(Level lv)
         {
             foreach(ref e; loggers)
                 e.thrLv = lv;
@@ -200,22 +227,23 @@ unittest
     import std.array;
 
     auto app = appender!string();
-    logger!(graphite.utils.log).bind(app);
-    logger!(graphite.utils.log).thrLv = Logger.Level.warning;
+    logger.bind(app);
+    logger.thrLv = Level.warning;
 
-    logger!(graphite.utils.log).write!("warning", "file", 0, "func")("foo");
-    assert(app.data == `[warning]<log>{file(0): func}: foo`);
+    logger("file", 0, "func").write!("warning")("foo");
+    assert(app.data == `[warning]<"graphite.utils.log">{file(0): func}: foo`);
 
-    logger!(graphite.utils.log).write!("notice", "file", 1, "func")("bar");
-    assert(app.data == `[warning]<log>{file(0): func}: foo`);
+    logger("file", 1, "func").write!("notice")("bar");
+    assert(app.data == `[warning]<"graphite.utils.log">{file(0): func}: foo`);
 
-    logger!(graphite.utils.log).write!("fatalError", "f", 2, "p")("hoge");
-    assert(app.data == "[warning]<log>{file(0): func}: foo[fatalError]<log>{f(2): p}: hoge");
+    logger("f", 2, "p").write!("fatalError")("hoge");
+    assert(app.data == `[warning]<"graphite.utils.log">{file(0): func}: foo[fatalError]<"graphite.utils.log">{f(2): p}: hoge`);
 }
 
 unittest
 {
     import std.array;
+    import std.stdio;
 
     // 使い方
     // logger!identifier()のデフォルト設定は、logger()から引き継がれる
@@ -227,9 +255,9 @@ unittest
 
     // logger!(identifier)のデフォルトの設定は、グローバル変数のloggers[0]から受け継がれる
     // 今回はlevelをnoticeにしてみる。
-    logger!"logTest".thrLv = Logger.Level.notice;
+    logger!"logTest".thrLv = Level.notice;
 
     // ログに出力してみる
     logger!"logTest".writeln!"warning"("まじかよ…");        // こんな感じで使う
-    logger!"unittest".writefln!"verbose"("{%s}", app.data); // 例えばこんな感じ
+    //logger!"unittest".writefln!"verbose"("{%s}", app.data); // 例えばこんな感じ
 }
