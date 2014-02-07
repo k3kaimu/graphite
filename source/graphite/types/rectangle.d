4c5a3702e7b12dc81bf7df8db9202d926e2f8d11
k3_kaimu <enjouzensyou.boinc@gmail.com>
2014-02-07 17:00:01 +0900

    rectangle.dの実装完了(未テスト)

--- source/graphite/types/rectangle.d
+++ source/graphite/types/rectangle.d
@@ -1,6 +1,9 @@
 module graphite.types.rectangle;
 
-import graphite.math.matrix;
+import graphite.math;
+import graphite.types.point;
+import graphite.utils.constants;
+
 
 struct Rectangle
 {
@@ -17,10 +20,9 @@ struct Rectangle
     /**
     1つの位置ベクトルと、矩形の縦横の大きさを指定して作成
     */
-    this(T)(T pos, float w, float h)
-    if(is(typeof({_p = pos;})))
+    this(Point pos, float w, float h)
     {
-        _p = pos;
+        _p = pos.vec.swizzle.xy;
         _wh = [w, h];
     }
 
@@ -37,6 +39,12 @@ struct Rectangle
 
         _p = [x0, x1];
         _wh = [x1 - x0, y1 - y0];
+    }
+
+
+    static typeof(this) fromCenter(in Point p, float w, float h)
+    {
+        return typeof(this)(p.x - w / 2, p.y - h / 2, w, h);
     }
 
 
@@ -152,10 +160,467 @@ struct Rectangle
 
         _p = [newW, newH];
         _p *= -1 / 2.0;
-        _p += c;
+        _p = _p + c.vec.swizzle.xy;
 
-        _w = newW;
-        _h = newH;
+        this._wh = [newW, newH];
+    }
+
+
+    void scaleTo(in Rectangle tarRect,
+                 ScaleMode scaleMode = ScaleMode.fit)
+    {
+        final switch(scaleMode){
+          case ScaleMode.fit:
+            scaleTo(tarRect, AspectRatioMode.keep,
+                             AlignHorz.center,
+                             AlignVert.center);
+            break;
+
+          case ScaleMode.fill:
+            scaleTo(tarRect, AspectRatioMode.keepByExpanding,
+                             AlignHorz.center,
+                             AlignVert.center);
+            break;
+
+          case ScaleMode.center:
+            alignTo(tarRect, AlignHorz.center,
+                             AlignVert.center);
+            break;
+
+          case ScaleMode.stretchToFill:
+            scaleTo(tarRect, AspectRatioMode.ignore,
+                             AlignHorz.center,
+                             AlignVert.center);
+            break;
+        }
+    }
+
+
+    void scaleTo(in Rectangle tarRect,
+                 AspectRatioMode subjectAspectRatioMode,
+                 AlignHorz sharedHorzAnchor = AlignHorz.center,
+                 AlignVert sharedVertAnchor = AlignVert.center)
+    {
+        scaleTo(tarRect,
+                subjectAspectRatioMode,
+                sharedHorzAnchor,
+                sharedVertAnchor,
+                sharedHorzAnchor,
+                sharedVertAnchor);
+    }
+
+
+
+
+    void scaleTo(in Rectangle tarRect,
+                 AspectRatioMode subjectAspectRatioMode,
+                 AlignHorz modelHorzAnchor,
+                 AlignVert modelVertAnchor,
+                 AlignHorz subjectHorzAnchor,
+                 AlignVert subjectVertAnchor)
+    {
+        immutable real tw = tarRect.width,
+                       th = tarRect.height,
+                       sw = this.width,
+                       sh = this.height,
+                       rw = tw / sw,
+                       rh = th / sh;
+
+        final switch(subjectAspectRatioMode)
+        {
+          case AspectRatioMode.keepByExpanding, AspectRatioMode.keep:
+            if(subjectAspectRatioMode == AspectRatioMode.keepByExpanding){
+                scale(.max(rw, rh));
+            }else{
+                scale(.min(rw, rh));
+            }
+
+            break;
+
+          case AspectRatioMode.ignore:
+            this._wh = tarRect._wh;
+            break;
+        }
+
+
+        alignTo(tarRect,
+                modelHorzAnchor,
+                modelVertAnchor,
+                subjectHorzAnchor,
+                subjectVertAnchor);
+    }
+
+
+    void alignToHorz(float tarX,
+                     AlignHorz thisHorzAnchor = AlignHorz.center)
+    in{
+        assert(thisHorzAnchor != AlignHorz.ignore);
+    }
+    body{
+        translateX(tarX - this.horzAnchor(thisHorzAnchor));
+    }
+
+
+    void alignToHorz(in Rectangle tarRect,
+                     AlignHorz sharedAnchor = AlignHorz.center)
+    {
+        alignToHorz(tarRect, sharedAnchor, sharedAnchor);
+    }
+
+
+    void alignToHorz(in Rectangle tarRect,
+                     AlignHorz tarHorzAnchor,
+                     AlignHorz thisHorzAnchor)
+    in{
+        assert(tarHorzAnchor != AlignHorz.ignore);
+        assert(thisHorzAnchor != AlignHorz.ignore);
+    }
+    body{
+        alignToHorz(tarRect.horzAnchor(tarHorzAnchor), thisHorzAnchor);
+    }
+
+
+    void alignToVert(float tarY,
+                     AlignVert sharedAnchor = AlignVert.center)
+    in{
+        assert(sharedAnchor != AlignVert.ignore);
+    }
+    body{
+        translateY(tarY - this.vertAnchor(sharedAnchor));
+    }
+
+
+    void alignToVert(in Rectangle tarRect,
+                     AlignVert sharedAnchor = AlignVert.center)
+    {
+        alignToVert(tarRect, sharedAnchor, sharedAnchor);
+    }
+
+
+    void alignToVert(in Rectangle tarRect,
+                     AlignVert tarVertAnchor,
+                     AlignVert thisVertAnchor)
+    in{
+        assert(tarVertAnchor != AlignVert.ignore);
+        assert(thisVertAnchor != AlignVert.ignore);
+    }
+    body{
+        alignToVert(tarRect.vertAnchor(tarVertAnchor), thisVertAnchor);
+    }
+
+
+    void alignTo(in Point tarPoint,
+                AlignHorz thisHorzAnchor = AlignHorz.center,
+                AlignVert thisVertAnchor = AlignVert.center)
+    {
+        alignToHorz(tarPoint.x, thisHorzAnchor);
+        alignToVert(tarPoint.y, thisVertAnchor);
+    }
+
+
+    void alignTo(in Rectangle tarRect,
+                 AlignHorz sharedHorzAnchor = AlignHorz.center,
+                 AlignVert sharedVertAnchor = AlignVert.center)
+    {
+        alignTo(tarRect,
+                sharedHorzAnchor,
+                sharedVertAnchor,
+                sharedHorzAnchor,
+                sharedVertAnchor);
+    }
+
+    void alignTo(in Rectangle tarRect,
+                 AlignHorz tarHorzAnchor,
+                 AlignVert tarVertAnchor,
+                 AlignHorz thisHorzAnchor,
+                 AlignVert thisVertAnchor)
+    {
+        alignToHorz(tarRect, tarHorzAnchor, thisHorzAnchor);
+        alignToVert(tarRect, tarVertAnchor, thisVertAnchor);
+    }
+
+
+    bool inside(in Point p) const
+    {
+        return p.x > this.minX && p.y > this.minY &&
+               p.x < this.maxY && p.y < this.maxY;
+    }
+
+
+    bool inside(in Rectangle rect) const
+    {
+        return this.minX < rect.maxX && this.maxX > rect.minX &&
+               this.minY < rect.maxY && this.maxY > rect.minY;
+    }
+
+
+    bool inside(in Point p0, in Point p1) const
+    {
+        return inside(p0) && inside(p1);
+    }
+
+
+
+    bool intersects(in Rectangle rect) const
+    {
+        return this.minX < rect.maxX && this.maxX > rect.minX &&
+               this.minY < rect.maxY && this.maxY > rect.minY;
+    }
+
+
+    bool intersects(in Point p0, in Point p1) const
+    {
+        Point p;
+
+        return inside(p0) ||
+               inside(p1) ||
+               lineSegmentIntersection(p0, p1, topLeft, topRight, p) ||
+               lineSegmentIntersection(p0, p1, topRight, bottomRight, p) ||
+               lineSegmentIntersection(p0, p1, bottomRight, bottomLeft, p) ||
+               lineSegmentIntersection(p0, p1, bottomLeft, topLeft, p);
+    }
+
+
+    void growToInclude(in Point p)
+    {
+        growToInclude(Rectangle(p, 0, 0));
+    }
+
+
+    void growToInclude(in Rectangle rect)
+    {
+        immutable x0 = .min(this.minX, rect.minX),
+                  x1 = .max(this.maxX, rect.maxX),
+                  y0 = .min(this.minY, rect.minY),
+                  y1 = .max(this.maxY, rect.maxY);
+
+        _p = [x0, y0];
+        _wh = [x1 - x0, y1 - y0];
+    }
+
+
+    void growToInclude(in Point p0, in Point p1)
+    {
+        growToInclude(p0);
+        growToInclude(p1);
+    }
+
+
+    Rectangle getIntersection(in Rectangle rect) const
+    {
+        immutable x0 = .max(this.minX, rect.minX),
+                  x1 = .min(this.maxX, rect.maxX),
+                  w = x1 - x0;
+
+        if(w < 0)
+            return Rectangle(0, 0, 0, 0);
+
+        immutable y0 = .max(this.minY, rect.minY),
+                  y1 = .min(this.maxY, rect.maxY),
+                  h = y1 - y0;
+
+        if(h < 0)
+            return Rectangle(0, 0, 0, 0);
+
+        return Rectangle(x0, y0, w, h);
+    }
+
+
+    Rectangle getUnion(in Rectangle rect) const
+    {
+        Rectangle r = this;
+        r.growToInclude(rect);
+        return r;
+    }
+
+
+    void standardize()
+    {
+        if(width < 0){
+            x += width;
+            width = -width;
+        }
+
+        if(height < 0){
+            y += height;
+            height = -height;
+        }
+    }
+    
+
+    Rectangle standardized() const @property
+    {
+        if(this.isStandardized)
+            return this;
+        else{
+            Rectangle rect = this;
+            rect.standardize();
+            return rect;
+        }
+    }
+
+
+    bool isStandardized() const @property
+    {
+        return width >= 0 && height >= 0;
+    }
+
+
+    float area() const @property
+    {
+        return abs(width * height);
+    }
+
+
+    float perimeter() const @property
+    {
+        return abs(width) * 2 + abs(height) * 2;
+    }
+
+
+    float aspectRatio() const @property
+    {
+        return abs(width / height);
+    }
+
+
+    bool empty() const @property
+    {
+        return width == 0 && height == 0;
+    }
+
+
+    Point min() const @property
+    {
+        return Point(minX, minY);
+    }
+
+
+    Point max() const @property
+    {
+        return Point(maxX, maxY);
+    }
+
+
+    float minX() const @property
+    {
+        return .min(x, x + width);
+    }
+
+
+    float maxX() const @property
+    {
+        return .max(x, x + width);
+    }
+
+
+    float minY() const @property
+    {
+        return .min(y, y + height);
+    }
+
+
+    float maxY() const @property
+    {
+        return .max(y, y + height);
+    }
+
+
+    float left()   const @property
+    {
+        return minX;
+    }
+
+
+    float right()  const @property
+    {
+        return maxX;
+    }
+
+
+    float top()    const @property
+    {
+        return minY;
+    }
+
+
+    float bottom() const @property
+    {
+        return maxY;
+    }
+
+    
+    Point topLeft() const @property
+    {
+        return min;
+    }
+
+    Point topRight() const @property
+    {
+        return Point(right, top);
+    }
+
+
+    Point bottomLeft() const @property
+    {
+        return Point(left, bottom);
+    }
+
+
+    Point bottomRight() const @property
+    {
+        return max;
+    }
+
+
+    float horzAnchor(AlignHorz anchor) const
+    in{
+        assert(anchor != AlignHorz.ignore);
+    }
+    body{
+        final switch(anchor)
+        {
+          case AlignHorz.left:
+            return left;
+          case AlignHorz.right:
+            return right;
+          case AlignHorz.center:
+            return center.x;
+          case AlignHorz.ignore:
+            return 0.0;
+        }
+    }
+
+
+    float vertAnchor(AlignVert anchor) const
+    in{
+        assert(anchor != AlignVert.ignore);
+    }
+    body{
+        final switch(anchor)
+        {
+          case AlignVert.top:
+            return top;
+          case AlignVert.bottom:
+            return bottom;
+          case AlignVert.center:
+            return center.y;
+          case AlignVert.ignore:
+            return 0.0;
+        }
+    }
+
+
+    Point center() const @property
+    {
+        return Point(x + width / 2, y + height / 2);
+    }
+
+
+    Rectangle opBinary(string op : "+")(in Point p) const
+    {
+        Rectangle rect = this;
+        rect._p = rect._p + p.vec.swizzle.xy;
     }
 
 
